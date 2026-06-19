1a82c3c2afead7c458b9a1fd411f683b5fc14c49
marat <marat.zamlet-97@yandex.ru>
2026-06-19 10:44:34 +0400

    скопировал содержимое в корень проекта для публикации

---
+++ scripts/index.js
@@ -0,0 +1,8 @@
+// source/scripts/index.js
+var navButton = document.querySelector(".navigation__button-close");
+var navDiv = document.querySelector(".navigation__menu");
+var map = document.querySelector(".contacts__map");
+navButton.classList.remove("navigation__button-close--nojs");
+navDiv.classList.remove("navigation__menu--nojs");
+map.classList.remove("contacts__map--nojs");
+//# sourceMappingURL=index.js.map
